361b7e700fbf3bdfcef5c7794561d4ec78c20637
Jelly1500 <58739539+Jelly1500@users.noreply.github.com>
2023-04-11 12:44:25 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup (
+  name = 'Sejin',
+  version = '0.0.1',
+  description='oss-dev my calculator 0001',
+  author = 'Heosejin',
+  url = "https://github.com/Jelly1500/calc0001",
+  download_url = "https://github.com/Jelly1500/calc0001/archive/refs/tags/v0.0.1.zip",
+  package = ['mycalc'],
+  classifiers=[
+    "Programing Language :: Python :: 3",
+  ]
+)
